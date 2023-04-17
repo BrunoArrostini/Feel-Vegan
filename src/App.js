54e1d27d28a4be897a8dfdd315e4259b577d4ee8
BrunoArrostini <email@github.com>
2023-04-17 13:00:28 +0200

    fetch added

--- src/App.js
+++ src/App.js
@@ -3,27 +3,9 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Card from './components/Card';
 import {Title, CardContainer} from "./components/styles/navbar.styled"
-import {nanoid} from "nanoid"
 import React from 'react';
 
 function App() {
-
-  const [recipe, setRecipes] = React.useState([]);
-      React.useEffect(() => {
-      getRecipes()
-      }, []);
-
-
-     const getRecipes = async () => {
-      const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${`c861de17b1ea4cd8bb78cc92a6cd4859`}&number=9&tags=vegetarian`);
-      const data = await api.json();
-     setRecipes(data.recipes);
-    };
-
-     const Cards = recipe.map((recipe=>{
-      <Card key={nanoid} title={recipe.name}  />
-    }))
 
   return (
     <div className="App">
@@ -31,12 +13,23 @@ function App() {
      <Title>Title demo blablabla</Title>
      <div className='main'>
       <CardContainer>
-        {Cards}
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </CardContainer>
      </div>
      <Footer />
     </div>
   );
-}
+ }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,30 @@ function Navbar() {
 
   const [formData, setFormData] = React.useState({data:""})
   
- function handleChange(e) {
+ const handleChange = (e) => {
     setFormData(e.target.value)
  } 
  
- function handleSubmit(e) {
+ const handleSubmit = (e) => {
     e.preventDefault();
+    getRecipes();
  }
 
+ const [recipe, setRecipes] = React.useState([]);
+      React.useEffect(() => {
+      getRecipes()
+      }, []);
+
+
+     const getRecipes = async () => {
+      const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${`c861de17b1ea4cd8bb78cc92a6cd4859`}&number=9&tags=vegetarian`);
+      const data = await api.json();
+     setRecipes(data.recipes);
+    };
+
  return(
+
      <Wrapper>
        <img src={vegan} style={{
         width: "100%",
